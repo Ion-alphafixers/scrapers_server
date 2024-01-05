9d1a2fbc9ce9ce18a4cb0cd568c92c98f6208a5d
jatannoury <jatannoury01@gmail.com>
2024-01-05 21:39:19 +0200

    FIX | headless

--- altline/scraper.py
+++ altline/scraper.py
@@ -13,7 +13,7 @@ import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--window-size=1850x1000")
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("enable-automation")
